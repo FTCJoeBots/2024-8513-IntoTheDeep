b6f288c0e0a2281cb7c5838fd3e9590f65f1e3eb
mrlawler <80868603+mrlawler@users.noreply.github.com>
2024-10-08 18:35:50 -0400

    Update clawMaize.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/clawMaize.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/clawMaize.java
@@ -1,1 +1,29 @@
 
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class clawMaize
+{
+    //contants
+    public static final double CLAWCLOSED = 0.2;
+    public static final double CLAWOPENED = 0.4;
+
+    //variables
+    Servo clawServo = null;
+
+    //init
+    public void init (HardwareMap hwmap) {
+        //get the servo from the controlhub
+    }
+    //methods
+    public void openClaw () {
+        clawServo.setPosition(CLAWOPENED);
+    }
+    public void closedClaw() {
+        clawServo.setPosition(CLAWCLOSED);
+    }
+
+
+}
